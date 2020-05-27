d1f840b1b89a6ca35b0ee89586fdffde9204fb23
wanghuajian620 <154659041@qq.com>
2020-05-27 10:33:01 +0800

    设置页简单实现

--- oceanside/lib/pages/mine/index.dart
+++ oceanside/lib/pages/mine/index.dart
@@ -6,40 +6,44 @@ class Mine extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: 415,
-              height: 300,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(Constant.ASSETS_IMG + 'background.jpeg'),
-                  fit: BoxFit.cover
-              )),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage:
-                        AssetImage(Constant.ASSETS_IMG + 'avatar.jpeg'),
-                      radius: 50.0,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+            width: 415,
+            height: 300,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Constant.ASSETS_IMG + 'background.jpeg'),
+                fit: BoxFit.cover
+            )),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage:
+                      AssetImage(Constant.ASSETS_IMG + 'avatar.jpeg'),
+                    radius: 50.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Text(
+                      '点我登录',
+                      style: TextStyle(fontSize: 22.0, color: Colors.white),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15.0),
-                      child: Text(
-                        '点我登录',
-                        style: TextStyle(fontSize: 22.0, color: Colors.white),
-                      ),
-                    )
-                  ],
-              )),
-            ),
-            OceanList()
-          ],
-        ),
-    ));
+                  )
+                ],
+            )),
+          ),),
+          Expanded(
+            flex: 2,
+            child: OceanList()
+          )
+        ],
+      ),
+    );
   }
 }
--- oceanside/lib/pages/mine/list.dart
+++ oceanside/lib/pages/mine/list.dart
@@ -6,31 +6,31 @@ class OceanList extends StatelessWidget {
     return ListView(
       children: <Widget>[
         ListTile(
-          leading: Icon(Icons.favorite_border),
+          leading: Icon(Icons.favorite_border, color: Colors.purple[200],),
           title: Text('收藏'),
           trailing: Icon(Icons.keyboard_arrow_right),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.brightness_2),
+          leading: Icon(Icons.brightness_2, color: Colors.purple[200],),
           title: Text('黑夜模式'),
           trailing: Icon(Icons.keyboard_arrow_right),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.palette),
+          leading: Icon(Icons.palette, color: Colors.purple[200],),
           title: Text('色彩主题'),
           trailing: Icon(Icons.keyboard_arrow_down),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.settings),
+          leading: Icon(Icons.settings, color: Colors.purple[200],),
           title: Text('设置'),
           trailing: Icon(Icons.keyboard_arrow_right),
           onTap: () {},
         ),
         ListTile(
-          leading: Icon(Icons.error_outline),
+          leading: Icon(Icons.error_outline, color: Colors.purple[200],),
           title: Text('关于'),
           trailing: Icon(Icons.keyboard_arrow_right),
           onTap: () {},
