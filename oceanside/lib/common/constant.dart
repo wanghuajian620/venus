f5557def27b574131da4515b292f95f6975cab74
wanghuajian620 <154659041@qq.com>
2020-05-29 22:05:07 +0800

    切换主题色

--- oceanside/lib/common/constant.dart
+++ oceanside/lib/common/constant.dart
@@ -1,4 +1,24 @@
+import 'package:flutter/material.dart';
 
 class Constant {
   static const String ASSETS_IMG = 'assets/images/';
 }
+
+Map<String, Color> themeColorMap = {
+  'gray': Colors.grey,
+  'blue': Colors.blue,
+  'blueAccent': Colors.blueAccent,
+  'cyan': Colors.cyan,
+  'deepPurple': Colors.purple,
+  'deepPurpleAccent': Colors.deepPurpleAccent,
+  'deepOrange': Colors.orange,
+  'green': Colors.green,
+  'indigo': Colors.indigo,
+  'indigoAccent': Colors.indigoAccent,
+  'orange': Colors.orange,
+  'purple': Colors.purple,
+  'pink': Colors.pink,
+  'red': Colors.red,
+  'teal': Colors.teal,
+  'black': Colors.black,
+};
--- oceanside/lib/pages/mine/list.dart
+++ oceanside/lib/pages/mine/list.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:oceanside/model/theme_model.dart';
+import 'package:provider/provider.dart';
+import 'package:oceanside/common/constant.dart';
 
 class OceanList extends StatelessWidget {
+  dynamic _colorKey;
+  // dynamic _themeColor;
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -14,14 +19,53 @@ class OceanList extends StatelessWidget {
         ListTile(
           leading: Icon(Icons.brightness_2, color: Colors.purple[200],),
           title: Text('黑夜模式'),
-          trailing: Icon(Icons.keyboard_arrow_right),
+          // trailing: Icon(Icons.keyboard_arrow_right),
+          trailing: Switch(value: true, onChanged: null, activeColor: Colors.purple[200],activeTrackColor: Colors.purple[200],),
           onTap: () {},
         ),
-        ListTile(
-          leading: Icon(Icons.palette, color: Colors.purple[200],),
-          title: Text('色彩主题'),
-          trailing: Icon(Icons.keyboard_arrow_down),
-          onTap: () {},
+        // ListTile(
+        //   leading: Icon(Icons.palette, color: Colors.purple[200],),
+        //   title: Text('色彩主题'),
+        //   trailing: Icon(Icons.keyboard_arrow_down),
+        //   onTap: () {},
+        // ),
+        ExpansionTile(
+          leading: Icon(Icons.color_lens),
+          title: Text('颜色主题'),
+          initiallyExpanded: false,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Wrap(
+                spacing: 8,
+                runSpacing: 8,
+                children: themeColorMap.keys.map((key) {
+                  Color value = themeColorMap[key];
+                  return InkWell(
+                    onTap: () {
+                      // setState(() {
+                      //   _colorKey = key;
+                      // });
+                      // SpUtil.putString('key_theme_color', key);
+                      Provider.of<ThemeModel>(context, listen: false)
+                          .setTheme(_colorKey = key);
+                    },
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      color: value,
+                      child: _colorKey == key
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : null,
+                    ),
+                  );
+                }).toList(),
+              ),
+            )
+          ],
         ),
         ListTile(
           leading: Icon(Icons.settings, color: Colors.purple[200],),
