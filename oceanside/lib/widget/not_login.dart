336abe0ada8be5e24086ce6dd3961ec10a6239e5
wanghuajian620 <154659041@qq.com>
2020-06-10 23:33:42 +0800

    未登录页

--- oceanside/lib/widget/not_login.dart
+++ oceanside/lib/widget/not_login.dart
@@ -10,9 +10,36 @@ class NotLogin extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            CircleAvatar(
-              backgroundImage: AssetImage(Constant.ASSETS_IMG + 'avatar.jpeg'),
-              radius: 50.0,
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage:
+                      AssetImage(Constant.ASSETS_IMG + 'avatar.jpeg'),
+                    radius: 50.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Text(
+                      '未登录',
+                      style: TextStyle(fontSize: 15.0, color: Colors.grey),
+                    ),
+                  )
+                ],
+              )
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: RaisedButton(
+                  color: Colors.transparent,
+                  // elevation: 100,
+                  child: Text('登录', style: TextStyle(fontSize: 15)),
+                  onPressed: () {},
+                )
+              )
             )
           ],
         ),
